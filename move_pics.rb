aac7e029303e4b90cb7878b8bb6e4faa07c831a8
dotob <sk@dotob.de>
2014-07-31 09:25:29 +0200

    fix year access, new version with commander

--- move_pics.rb
+++ move_pics.rb
@@ -9,35 +9,37 @@ if @just_fake_it
 end
 
 def doit
-	for imgFile in Dir.glob("*.{jpg,JPG}") do
-		# try exi date first
-		imgFileDate = EXIFR::JPEG.new(imgFile).date_time
-		# then filedate
-		imgFileDate ||=	File.new(imgFile).mtime
-		if imgFileDate
-			create_dir_and_move(imgFileDate, imgFile) 
-		else
-			puts "not date in #{imgFile}"
-		end
-	end
+  for imgFile in Dir.glob("*.{jpg,JPG}") do
+    # try exi date first
+    imgFileDate = EXIFR::JPEG.new(imgFile).date_time
+    # then filedate
+    imgFileDate ||= File.new(imgFile).mtime
+    if !imgFileDate.nil?
+      create_dir_and_move(imgFileDate, imgFile) 
+    else
+      puts "not date in #{imgFile}"
+    end
+  end
 end
 
 def create_dir_and_move (imgFileDate, imgFile)
-	dateString = imgFileDate.year.to_s + "_" + sprintf("%02d", imgFileDate.month)
-	unless File.directory? dateString
-		Dir.mkdir dateString if !@just_fake_it 
-		puts "created dir #{dateString} #{@msg}" 
-	end
-	move_but_do_not_override(imgFile, dateString+"/"+imgFile)
+  if imgFileDate.respond_to?(:year)
+    dateString = imgFileDate.year.to_s + "_" + sprintf("%02d", imgFileDate.month)
+    unless File.directory? dateString
+      Dir.mkdir dateString if !@just_fake_it 
+      puts "created dir #{dateString} #{@msg}" 
+    end
+    move_but_do_not_override(imgFile, dateString+"/"+imgFile)
+  end
 end
 
 def move_but_do_not_override (imgFile, destination)
-	if File.exists?(destination)
-		puts "#{imgFile}  NOT MOVED, WOULD OVERRIDE #{@msg}"
-	else
-		puts "#{imgFile}  >>  #{destination} #{@msg}"
-		FileUtils.mv(imgFile, destination) if !@just_fake_it
-	end
+  if File.exists?(destination)
+    puts "#{imgFile}  NOT MOVED, WOULD OVERRIDE #{@msg}"
+  else
+    puts "#{imgFile}  >>  #{destination} #{@msg}"
+    FileUtils.mv(imgFile, destination) if !@just_fake_it
+  end
 end
 
 doit
